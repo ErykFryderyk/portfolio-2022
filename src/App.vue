f0b2ed4bac4c981a7cd107deb6616f117be791bf
Eryk Fryderyk <grzeniu098@gmail.com>
2022-12-04 23:07:57 +0100

    import font styles

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,13 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: black
 }
 
 #nav {
