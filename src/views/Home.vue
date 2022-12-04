cefac5a73360bb8e1887a07efa77aed149d6c31f
Eryk Fryderyk <grzeniu098@gmail.com>
2022-12-04 23:08:45 +0100

    add content to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HI THERE!</h1>
+    <h2>I'M GRZEGORZ</h2>
+    <p>Junior FRONTEND DEVELOPER</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius adipisci eaque, dolores, perspiciatis maxime nemo, cumque quasi blanditiis ipsum sint alias odio consequatur rerum excepturi autem? Neque iure impedit explicabo.</p>
+    <button>More About Me</button>
+    <PrimaryButton>More About Me!</PrimaryButton>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import PrimaryButton from '@/components/PrimaryButton.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    PrimaryButton
   }
 }
 </script>
