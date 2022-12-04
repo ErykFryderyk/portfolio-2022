2288494348eaace59f1210e1b37aa0105b866c14
Eryk Fryderyk <grzeniu098@gmail.com>
2022-12-04 23:08:16 +0100

    create primary button

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="primary-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+  props: {
+    textButton: String,
+  }
+};
+</script>
+
+<style>
+.primary-btn {
+  border: none;
+  background-color: #46a2ca;
+  border-radius: 50px;
+  color: white;
+  font-size: 16px;
+  text-transform: uppercase;
+  font-weight: bold;
+  padding: 12px 30px;
+  -webkit-box-shadow: 3px 3px 24px 0px rgba(200, 200, 200, 1);
+  -moz-box-shadow: 3px 3px 24px 0px rgba(200, 200, 200, 1);
+  box-shadow: 3px 3px 10px 0px rgba(200, 200, 200, 1);
+  cursor: pointer;
+  letter-spacing: 1px;
+}
+</style>
